b266bc94a54b79117c4c5554b457f13d3702c8c4
Oleg Verych <olecom@gmail.com>
2014-10-04 15:40:10 +0300

    global.log(): optimize for V8

--- app_main/app_back.js
+++ app_main/app_back.js
@@ -1,7 +1,10 @@
 (function uglify_js_closure(global, process, con){
 var cfg = require('./lib/read_config.js')
 
-    global.log = function log(){ con.log.apply(con, arguments)}
+    global.log = function log(a, b, c){
+        if(b && c) con.log(a, b, c)
+        else b &&  con.log(a, b) || con.log(a)
+    }
 
     require('./lib/response.js')
     require('./lib/process.js')(global, process)
