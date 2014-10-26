38c8b6d2e86d1119e4b3cf0fc60a0fc703b17404
Oleg Verych <olecom@gmail.com>
2014-10-26 07:32:13 +0300

    uncaughtException: log in push
    Signed-off-by: Oleg Verych <olecom@gmail.com>

--- app_main/lib/process.js
+++ app_main/lib/process.js
@@ -5,13 +5,12 @@ function processSETUP(global, process, uncaughtExceptions){
 
     global.__res = null// catch request to inform UI, see note in 'uncaughtException'
     global.pushUncaughtException = function(that){
+        log('!Caught:', that)
         uncaughtExceptions.push(that)
     }
 
     process.on('uncaughtException', function(err){
         pushUncaughtException(err.stack)
-        log('Caught exception: ' + err.stack)
-
         try { if(__res){
     // NOTE: with many users and high load this may send errors to
     //       the wrong end; thus this is mostly a development tool
