67042ade408aa903a8681680cd96a754bf63b502
Oleg Verych <olecom@gmail.com>
2014-11-02 06:30:51 +0400

    mw/sendFile: noop refactor and coding style
    Signed-off-by: Oleg Verych <olecom@gmail.com>

--- app_main/lib/middleware/sendFile.js
+++ app_main/lib/middleware/sendFile.js
@@ -1,36 +1,36 @@
 module.exports = function sendFile(name, absolute){
-var fs = require('fs')
-   ,api  = require('../api.js')
-    return function sendFile(req, res){
-    var fstream = fs.createReadStream((absolute ? '' : __dirname + '/../../') + name)
+    return function sendFile(r__, res){
+    var fs = require('fs')
+       ,fstream = fs.createReadStream((absolute ? '' : __dirname + '/../../') + name)
+
         fstream.on('open',
-            function on_fstream_open(fd){
-                fs.fstat(fd,
-                    function on_fstat(err, stat){
-                        if(err){
-                            res.statusCode = 500
-                            log('sendFile err: ', err)
-                            res.txt(err.code || String(err))
-                            return
-                        }
-                        res.setHeader('Content-Length', stat.size)
-                        res.setHeader('Content-Type',(
-                           ~name.indexOf('css') ? res.ContentTypes.TextCSS :
-                           ~name.indexOf('.js') ? res.ContentTypes.AppJS :
-                            res.ContentTypes.TextPlain)['Content-Type']
-                        )
-                        fstream.pipe(res)
-                        return
-                    }
+        function on_fstream_open(fd){
+            return fs.fstat(fd,
+            function on_fstat(err, stat){
+                if(err){
+                    res.statusCode = 500
+                    log('sendFile err: ', err)
+                    res.txt(err.code || String(err))
+                    return
+                }
+                res.setHeader('Content-Length', stat.size)
+                res.setHeader('Content-Type',(
+                   ~name.indexOf('css') ? res.ContentTypes.TextCSS :
+                   ~name.indexOf('.js') ? res.ContentTypes.AppJS :
+                    res.ContentTypes.TextPlain)['Content-Type']
                 )
+                fstream.pipe(res)
+                return
             }
+            )
+        }
         )
         fstream.on('error',
-            function on_fstream_error(err){
-                res.statusCode = 500
-                log(err)
-                return res.txt(err.code || String(err))
-            }
+        function on_fstream_error(err){
+            res.statusCode = 500
+            log(err)
+            return res.txt(err.code || String(err))
+        }
         )
     }
 }
