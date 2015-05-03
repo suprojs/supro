472dcfe047b955d863cd85e881e1e8335a4d9aa9
Oleg Verych <olecom@gmail.com>
2015-05-04 00:19:32 +0300

    _strip_ExtJS4_nw.sh: strip_whitespace() && split_extjs()
    * move whitespace processing in function
    * fix license header in output, include GNU GPL line
    * add splitting of ext-all-debug into the "lite" and the "rest" parts
      file 'extjs-classes.txt' is a configuration of what classes are:
      -- left in "lite",
      -- moved to "rest",
      -- deleted/purged (unused at all);
    
    Resulting output:
    ```
    ext-lite-nw.js:   998 128 bytes
    ext-rest-nw.js: 1 192 071 bytes
    ```
    
    Signed-off-by: Oleg Verych <olecom@gmail.com>

--- _strip_ExtJS4_nw.sh
+++ _strip_ExtJS4_nw.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-# cleanup whitespace and leaked in comments of our git version of `ext-all-debug.js`
+
+[ "$1" = '?' -o "$1" = '-h' -o "$1" = '--help' ] && exec sed '' <<'!'
+# '$1':
+# * 'justsplit': split `ext-all-debug.js` into EXTJSLITE and EXTJSREST and exit
+# * 'strip': don't split
+# * ''/'*': do all
+# cleanup whitespace and leaked in comments of our git's `ext-all-debug.js`
 # save ~1.4M of file size
 # otherwise use `ext-all-debug.js` from same path as output/source
+!
 
 set -e
 
@@ -12,6 +19,13 @@ set +e
 trap "" 0
 exit 0
 ' 0 # catch errors
+
+JSFILE='ext-all-debug.js'
+NWFILE="ext-all-nw.js"
+
+EXTJSLITE='ext-lite'
+EXTJSREST='ext-rest'
+EXTJSCLASSES='extjs-classes.txt'
 
 CWD="./$0"
 CWD="${CWD%/*}" # get path to supro dir where this script must be in
@@ -34,6 +48,105 @@ $ git clone git://github.com/suprojs/extjs-4.2
     exit 1
 }
 
+#### whitespace ####
+
+strip_whitespace(){
+# $1 -- stdin
+# $2 -- stdout
+# also delete leaked in comments (usually after some REs: 'compileCRe', 'tagRe', etc.)
+# use simple '/^[/*]/d' for that as lines are whitespace clean
+# and no such symbol may start source code line (except curved RegExp assignments)
+
+    echo "
+= strip front whitespace =
+INPUT:  '$1'
+OUTPUT: '$2'
+" >&2
+
+    sed '
+1b
+8b
+20b
+5,15d
+19s/.*/Source: '"$JSFILE"' ('"$HEAD"')/
+/^[[:blank:]]*$/d
+/^[[:blank:]]/s/[[:blank:]]*//
+/^[/*]/d
+' <"$1" >"$2"
+
+    FSIZE=`du -b "$1"`
+    FSIZE=${FSIZE%%?[/.]*}
+    oIFS=`du -b "$2"`
+    echo "size: $FSIZE diff: $(( ($FSIZE - ${oIFS%%?[/.]*})/1024 )) kibytes" >&2
+# strip filename from `du` output where tab[\t] char is a separator:
+# 2218376[\t]../../../extjs-4.2/ext-all-nw.js
+# 2218376[\t]/d/extjs-4.2/ext-all-nw.js
+#        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^= ${oIFS%%?[/.]*}
+}
+
+#### split ####
+
+sed_skip_class(){
+# $1 -- class name
+# make `sed` code to skip class in "$1"
+    echo "
+/^Ext.define('$1/,/^});/b
+"
+}
+
+sed_mv_class(){
+# $1 -- class name
+# $2 -- result postfix ('-nw.js') from `split_extjs()`
+# make `sed` code to remove class in "$1"
+    echo "
+/^Ext.define('$1/,/^});/{
+    w $EXTJS4$EXTJSREST$2
+    d
+}
+"
+}
+
+sed_rm_class(){
+    echo "
+/^Ext.define('$1/,/^});/d
+"
+}
+
+process_classes(){
+# read lines from $RMCLASSES and construct `sed` code
+    oIFS=$IFS
+    IFS=`printf '\n_'`
+    IFS=${IFS%_} # `\n=LF` as the field separator
+    for i in `sed '/^[# ]/d' <"$EXTJSCLASSES"` # skip comment and whitespace lines
+    do case "$i" in
+    '~'*) sed_rm_class "${i#?}";; # >/dev/null
+    '!'*) sed_skip_class "${i#?}";; # skip
+    *) sed_mv_class "$i" "$1";;
+       esac
+    done
+    IFS=$oIFS
+}
+
+split_extjs(){
+    echo "
+= ExtJS lite / the rest =
+INPUT: '$EXTJS4$JSFILE'
+LITE:  '$EXTJS4$EXTJSLITE$1'
+REST:  '$EXTJS4$EXTJSREST$1'
+" >&2
+
+# $1 -- source postfix ('-debug.js')
+# NOTE: general pattern has some exclusions
+#' Ext.define('Ext.menu.ColorPicker', {'
+# ^ there are such lines
+    sed "
+/^ Ext[.]define/s_^ __
+`process_classes ""$1""`
+" <"$EXTJS4$JSFILE" >"$EXTJS4$EXTJSLITE$1"
+}
+
+#### main run ####
+
 OUTPUT=`sed '' <./extjs.txt`
 [ -d "$OUTPUT" ] || {
     echo "Error: no output '$OUTPUT' directory present"
@@ -50,37 +163,30 @@ EXTJS4=$OUTPUT
     trap '' 0
     exit 1
 }
-# output e.g. "/d/extjs-4.2/ext-all-nw.js" NOTE: this is mingw path
-OUTPUT="${OUTPUT}ext-all-nw.js"
+
 echo "
-PWD:    '$PWD'
-OUTPUT: '$OUTPUT'
 EXTJS4: '$EXTJS4'
 HEAD:   '$HEAD'
-"
+PWD:    '$PWD'
+" >&2
 
-FSIZE=`du -b "${EXTJS4}ext-all-debug.js"`
+[ "$1" = "strip" ] || {
+    split_extjs '-debug.js'
+    strip_whitespace "$EXTJS4$EXTJSLITE-debug.js" "${EXTJS4}$EXTJSLITE-nw.js"
+    # copy copyright/license header into the rest file
+    sed '22q' <"$EXTJS4$EXTJSLITE-debug.js"  >"${EXTJS4}$EXTJSREST-nw.js"
+    sed ''    <"$EXTJS4$EXTJSREST-debug.js" >>"${EXTJS4}$EXTJSREST-nw.js"
+    mv  "${EXTJS4}$EXTJSREST-nw.js" "$EXTJS4$EXTJSREST-debug.js"
+    strip_whitespace "$EXTJS4$EXTJSREST-debug.js" "${EXTJS4}$EXTJSREST-nw.js"
 
-# delete whitespace
-# delete leaked in comments (usually after some REs: 'compileCRe', 'tagRe', etc.)
-#   use simple '/^[/*]/d' for that as lines are whitespace clean
-#   and no such symbol may start source code line
-sed '
-1b
-20b
-8,15d
-19s/.*/Source: ext-all-debug.js ('"$HEAD"')/
-/^[[:blank:]]*$/d
-/^[[:blank:]]/s/[[:blank:]]*//
-/^[/*]/d
-' < "${EXTJS4}ext-all-debug.js" >"$OUTPUT"
+    [ "$1" = "justsplit" ] && {
+        trap '' 0
+        exit 0
+    }
+}
 
-OUTPUT=`du -b "$OUTPUT"`
-# strip filename from `du` output where tab[\t] char is a separator:
-# 2218376[\t]../../../extjs-4.2/ext-all-nw.js
-# 2218376[\t]/d/extjs-4.2/ext-all-nw.js
-#        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^= ${OUTPUT%%?[/.]*}
-echo "size diff: $(( (${FSIZE%%?[/.]*} - ${OUTPUT%%?[/.]*})/1024 )) kibytes"
+# output e.g. "/d/extjs-4.2/ext-all-nw.js" NOTE: this is mingw path
+strip_whitespace "$EXTJS4$JSFILE" "$OUTPUT$NWFILE"
 
 trap '' 0
 exit 0
